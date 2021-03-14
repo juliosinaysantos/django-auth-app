066fbd413a7316bdb41d26507a832918b813b2b2
Julio Sinay <juliosinaysantos@outlook.com>
2021-03-14 15:14:07 -0600

    feat: add user registration

--- users/forms/auth.py
+++ users/forms/auth.py
@@ -1,7 +1,36 @@
 from django import forms
 
+from users.models import User
+
 
 class LoginForm(forms.Form):
 
     login = forms.CharField()
     password = forms.CharField()
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(min_length=8)
+    password_confirmation = forms.CharField(min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirmation']
+
+    def clean_password_confirmation(self):
+        password = self.cleaned_data.get('password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+
+        if password != password_confirmation:
+            raise forms.ValidationError(
+                'Password do not match',
+                code='password_confirmation',
+            )
+        return password_confirmation
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(user.password)
+        if commit:
+            user.save()
+        return user
